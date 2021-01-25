5564099d671e691e1b4dd63f09ae72b392e45631
bignardi <vinicius.bignardi@outlook.com>
2021-01-25 18:27:28 -0300

    Service layer, component registration

--- src/main/java/com/vbt/spring/resources/UserResource.java
+++ src/main/java/com/vbt/spring/resources/UserResource.java
@@ -1,19 +1,34 @@
 package com.vbt.spring.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vbt.spring.entities.User;
+import com.vbt.spring.services.UserService;
 
 @RestController // Controlador Rest
 @RequestMapping(value = "/users") // Nome do recurso relacionado a entidade User
 public class UserResource {
 	
+	@Autowired // Injeção de dependência
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Maria", "maria@gmail.com", "999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/{id}") // Requisição do tipo GET
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }
---
+++ src/main/java/com/vbt/spring/services/UserService.java
@@ -0,0 +1,26 @@
+package com.vbt.spring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vbt.spring.entities.User;
+import com.vbt.spring.repositories.UserRepository;
+
+@Service // Registro da classe no mecânismo de injeção de dependência
+public class UserService {
+	
+	@Autowired // Injeção de dependência
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
