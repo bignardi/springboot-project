17923cc80ee241eda844241018399e139b1345b1
bignardi <vinicius.bignardi@outlook.com>
2021-02-25 20:38:23 -0300

    Payment, one-to-one association

--- src/main/java/com/vbt/spring/config/TestConfig.java
+++ src/main/java/com/vbt/spring/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.vbt.spring.entities.Category;
 import com.vbt.spring.entities.Order;
 import com.vbt.spring.entities.OrderItem;
+import com.vbt.spring.entities.Payment;
 import com.vbt.spring.entities.Product;
 import com.vbt.spring.entities.User;
 import com.vbt.spring.entities.enums.OrderStatus;
@@ -86,5 +87,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
 	}
 }
