df5bd211195b7976366f53499de518530d73dd16
bignardi <vinicius.bignardi@outlook.com>
2021-03-07 20:15:28 -0300

    User delete

--- src/main/java/com/vbt/spring/resources/UserResource.java
+++ src/main/java/com/vbt/spring/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,11 +36,19 @@ public class UserResource {
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	// Inserindo usuários
 	@PostMapping
 	public ResponseEntity<User>	insert(@RequestBody User obj) {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
 				.buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	// Deletando usuários por id
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.deleteById(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/vbt/spring/services/UserService.java
+++ src/main/java/com/vbt/spring/services/UserService.java
@@ -27,4 +27,8 @@ public class UserService {
 	public User insert(User obj) {
 		return repository.save(obj);
 	}
+	
+	public void deleteById(Long id) {
+		repository.deleteById(id);
+	}
 }
